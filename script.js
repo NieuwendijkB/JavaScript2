28d06a017e5178028088fd2a6f50802dcb6f408b
NieuwendijkB <93547503+NieuwendijkB@users.noreply.github.com>
2021-12-04 17:08:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hello Winc Academy students");
+// This is a large sum
+/*
+let multiplication = 1230941 * 1823794;
+console.log(multiplication);
+let divide = 637263 / 54;
+console.log(divide);
+*/
+// you cannot see the comment in the log because its a comment.
+/*same goes for the calculations in the commentblock
+because it works the same only on multiple lines*/
